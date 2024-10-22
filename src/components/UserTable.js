8806dab48d0f1898c4b07dbdcf84c57f1e6eb84f
Rishi <rishi.matam@gmail.com>
2024-10-22 12:43:53 +0530

    updated restApi

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -9,8 +9,14 @@ const UserTable = () => {
       <thead>
         <tr>
           <th>ID</th>
-          <th>Name</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>DOB</th>
+          <th>Gender</th>
           <th>Email</th>
+          <th>Full Address</th>
+          <th>Mobile</th>
+          <th>User Status</th>
           <th>Actions</th>
         </tr>
       </thead>
@@ -18,8 +24,14 @@ const UserTable = () => {
         {users.map(user => (
           <tr key={user.id}>
             <td>{user.id}</td>
-            <td>{user.name}</td>
+            <td>{user.first_name}</td>
+            <td>{user.last_name}</td>
+            <td>{user.dob}</td>
+            <td>{user.gender}</td>
             <td>{user.email}</td>
+            <td>{user.full_address}</td>
+            <td>{user.mobile}</td>
+            <td>{user.user_status}</td>
             <td>
               <button onClick={() => deleteUser(user.id)}>Delete</button>
               {/* Add edit functionality here */}
